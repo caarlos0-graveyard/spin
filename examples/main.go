a7d384953eec9fc6aaab7560706156f1b31a8803
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-12-12 10:19:50 -0200

    spinn

--- examples/main.go
+++ examples/main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	spin "github.com/caarlos0/spin-ext"
 )
 
 func main() {
-	spin := spin.New(`⦾⦿`, "%s Working...")
-	spin.Work(func() error {
-		time.Sleep(1000 * time.Millisecond)
-		return nil
+	show("Default", spin.Default)
+
+	show("Box1", spin.Box1)
+	show("Box2", spin.Box2)
+	show("Box3", spin.Box3)
+	show("Box4", spin.Box4)
+	show("Box5", spin.Box5)
+	show("Box6", spin.Box6)
+	show("Box7", spin.Box7)
+
+	show("Spin1", spin.Spin1)
+	show("Spin2", spin.Spin2)
+	show("Spin3", spin.Spin3)
+	show("Spin4", spin.Spin4)
+	show("Spin5", spin.Spin5)
+	show("Spin6", spin.Spin6)
+	show("Spin7", spin.Spin7)
+	show("Spin8", spin.Spin8)
+	show("Spin9", spin.Spin9)
+	show("Spin10", spin.Spin10)
+}
+
+func show(name, frames string) {
+	s := spin.New("  \033[36m[" + name + "] computing\033[m %s")
+	s.Set(frames)
+	s.Work(func() {
+		time.Sleep(100 * 10 * time.Millisecond)
 	})
-	fmt.Println("Done!")
 }
--- spin.go
+++ spin.go
@@ -3,40 +3,73 @@ package spin
 import (
 	"fmt"
 	"time"
-
-	tj "github.com/tj/go-spin"
 )
 
-const CLEAR_LINE = "\r\033[K"
+// ClearLine go to the beggining of the line and clear it
+const ClearLine = "\r\033[K"
+
+// Spinner types.
+var (
+	Box1    = `⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏`
+	Box2    = `⠋⠙⠚⠞⠖⠦⠴⠲⠳⠓`
+	Box3    = `⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆`
+	Box4    = `⠋⠙⠚⠒⠂⠂⠒⠲⠴⠦⠖⠒⠐⠐⠒⠓⠋`
+	Box5    = `⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠴⠲⠒⠂⠂⠒⠚⠙⠉⠁`
+	Box6    = `⠈⠉⠋⠓⠒⠐⠐⠒⠖⠦⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈`
+	Box7    = `⠁⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈⠈`
+	Spin1   = `|/-\`
+	Spin2   = `◴◷◶◵`
+	Spin3   = `◰◳◲◱`
+	Spin4   = `◐◓◑◒`
+	Spin5   = `▉▊▋▌▍▎▏▎▍▌▋▊▉`
+	Spin6   = `▌▄▐▀`
+	Spin7   = `╫╪`
+	Spin8   = `■□▪▫`
+	Spin9   = `←↑→↓`
+	Spin10  = `⦾⦿`
+	Default = Box1
+)
 
 // Spinner main type
 type Spinner struct {
-	spin   *tj.Spinner
-	text   string
+	frames []rune
+	length int
+	pos    int
 	active bool
+	text   string
 }
 
 // New Spinner with args
-func New(frames, text string) *Spinner {
-	spin := tj.New()
-	spin.Set(frames)
-	return &Spinner{
-		spin: spin,
-		text: CLEAR_LINE + text,
+func New(text string) *Spinner {
+	s := &Spinner{
+		text: ClearLine + text,
 	}
+	s.Set(Default)
+	return s
+}
+
+// Set frames to the given string which must not use spaces.
+func (s *Spinner) Set(frames string) {
+	s.frames = []rune(frames)
+	s.length = len(s.frames)
 }
 
 // Work shows the spinner, execute the given task and then hide the spinner
-func (s *Spinner) Work(task func() error) error {
+func (s *Spinner) Work(task func()) {
 	s.active = true
 	go func() {
 		for s.active {
-			fmt.Printf(s.text, s.spin.Next())
+			fmt.Printf(s.text, s.next())
 			time.Sleep(100 * time.Millisecond)
 		}
 	}()
-	err := task()
+	task()
 	s.active = false
-	fmt.Printf(CLEAR_LINE)
-	return err
+	fmt.Printf(ClearLine)
+}
+
+func (s *Spinner) next() string {
+	r := s.frames[s.pos%s.length]
+	s.pos++
+	return string(r)
 }
